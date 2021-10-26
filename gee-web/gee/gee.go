7ca9a61bddb99be221854c2e1c51c08add44e5bc
lidongyooo <2271439559@qq.com>
2021-10-26 10:35:50 +0800

    gee-web day four

--- gee-web/gee/gee.go
+++ gee-web/gee/gee.go
@@ -1,30 +1,55 @@
 package gee
 
 import (
-	"log"
 	"net/http"
 )
 
 type HandlerFunc func(c *Context)
 
+type RouterGroup struct {
+	prefix string
+	engine *Engine
+	middlewares []HandlerFunc
+}
+
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
 }
 
 func New() *Engine {
-	log.SetFlags(log.Lshortfile)
-
-	return &Engine{
+	engine := &Engine{
 		router: newRouter(),
 	}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+
+	return engine
 }
 
-func (engine *Engine) GET (pattern string, handler HandlerFunc) {
-	engine.router.addRoute("GET", pattern, handler)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		engine: engine,
+		prefix: group.prefix + prefix,
+	}
+	engine.groups = append(engine.groups, newGroup)
+
+	return newGroup
 }
 
-func (engine *Engine) POST (pattern string, handler HandlerFunc)  {
-	engine.router.addRoute("POST", pattern, handler)
+func (group *RouterGroup) addRoute(method, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET (pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST (pattern string, handler HandlerFunc)  {
+	group.addRoute("POST", pattern, handler)
 }
 
 

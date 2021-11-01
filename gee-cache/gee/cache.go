1c1b3fb97667bfc3752e784ab628cb8f7c3501c0
lidongyooo <2271439559@qq.com>
2021-11-01 10:45:22 +0800

    geecache days three

--- gee-cache/gee/cache.go
+++ gee-cache/gee/cache.go
@@ -1,9 +1,7 @@
 package gee
 
 import (
-	"fmt"
 	"gee-cache/lru"
-	"log"
 	"sync"
 )
 
@@ -11,84 +9,6 @@ type cache struct {
 	mu sync.Mutex
 	lru *lru.Cache
 	cacheBytes int64
-}
-
-type Getter interface {
-	Get(key string) ([]byte, error)
-}
-
-type GetterFunc func(key string) ([]byte, error)
-
-type Group struct {
-	name	string
-	getter  Getter
-	mainCache cache
-}
-
-var (
-	mu	sync.RWMutex
-	groups = make(map[string]*Group)
-)
-
-func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
-	log.SetFlags(log.Lshortfile)
-
-	if getter == nil {
-		panic("nil getter")
-	}
-
-	mu.Lock()
-	defer mu.Unlock()
-	g := &Group{
-		name:      name,
-		getter:    getter,
-		mainCache: cache{cacheBytes: cacheBytes},
-	}
-	groups[name] = g
-
-	return g
-}
-
-func (g *Group) Get(key string) (ByteView, error) {
-	if key == "" {
-		return ByteView{}, fmt.Errorf("key is required")
-	}
-
-	if v, ok := g.mainCache.get(key); ok {
-		log.Println("[GeeCache] hit")
-		return v, nil
-	}
-
-	return g.load(key)
-}
-
-func (g *Group) load(key string) (ByteView, error) {
-	return g.getLocally(key)
-}
-
-func (g *Group) getLocally(key string) (ByteView, error) {
-	bytes, err := g.getter.Get(key)
-	if err != nil {
-		return ByteView{}, err
-	}
-
-	value := ByteView{b: cloneBytes(bytes)}
-	g.populateCache(key, value)
-	return value, nil
-}
-
-func (g *Group) populateCache(key string, value ByteView)  {
-	g.mainCache.add(key, value)
-}
-
-func GetGroup(name string) *Group {
-	mu.RLock()
-	defer mu.RUnlock()
-	return groups[name]
-}
-
-func (f GetterFunc) Get(key string) ([]byte, error) {
-	return f(key)
 }
 
 func (c *cache) add(key string, value ByteView) {

80af1cb38f2aad3a2af94f3e8354af8e6c592d59
lidongyooo <2271439559@qq.com>
2021-11-18 11:15:48 +0800

    gee-cache 7 days

--- gee-cache/gee/peers.go
+++ gee-cache/gee/peers.go
@@ -1,10 +1,12 @@
 package gee
 
+import "gee-cache/pb"
+
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool)
 }
 
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
+	Get(in *pb.Request, out *pb.Response) error
 }
 
